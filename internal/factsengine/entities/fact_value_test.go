6e11fa10a8055462945ed03f3833e2e7089ba496
arbulu89 <xarbulu@suse.com>
2022-11-22 09:03:11 +0100

    Move fact_value tests to use a suite and _test package

--- internal/factsengine/entities/fact_value_test.go
+++ internal/factsengine/entities/fact_value_test.go
@@ -1,92 +1,105 @@
-package entities
+package entities_test
 
 import (
 	"testing"
 
-	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/suite"
+	"github.com/trento-project/agent/internal/factsengine/entities"
 )
 
-func TestFactValueAsInterface(t *testing.T) {
+type FactValueTestSuite struct {
+	suite.Suite
+}
+
+func TestFactValueTestSuite(t *testing.T) {
+	suite.Run(t, new(FactValueTestSuite))
+}
+
+func (suite *FactValueTestSuite) TestFactValueAsInterface() {
 	cases := []struct {
 		description string
-		factValue   FactValue
+		factValue   entities.FactValue
 		expected    interface{}
 	}{
 		{
 			description: "FactValueInt AsInterface",
-			factValue:   &FactValueInt{Value: 1},
+			factValue:   &entities.FactValueInt{Value: 1},
 			expected:    1,
 		},
 		{
 			description: "FactValueFloat AsInterface",
-			factValue:   &FactValueFloat{Value: 1.1},
+			factValue:   &entities.FactValueFloat{Value: 1.1},
 			expected:    1.1,
 		},
 		{
 			description: "FactValueString AsInterface",
-			factValue:   &FactValueString{Value: "test"},
+			factValue:   &entities.FactValueString{Value: "test"},
 			expected:    "test",
 		},
 		{
 			description: "FactValueBool AsInterface",
-			factValue:   &FactValueBool{Value: true},
+			factValue:   &entities.FactValueBool{Value: true},
 			expected:    true,
 		},
 		{
 			description: "FactValueMap AsInterface",
-			factValue:   &FactValueMap{Value: map[string]FactValue{"test": &FactValueString{Value: "test"}}},
-			expected:    map[string]interface{}{"test": "test"},
+			factValue: &entities.FactValueMap{
+				Value: map[string]entities.FactValue{
+					"test": &entities.FactValueString{Value: "test"}}},
+			expected: map[string]interface{}{"test": "test"},
 		},
 		{
 			description: "FactValueList AsInterface",
-			factValue:   &FactValueList{Value: []FactValue{&FactValueString{Value: "test"}}},
-			expected:    []interface{}{"test"},
+			factValue: &entities.FactValueList{
+				Value: []entities.FactValue{
+					&entities.FactValueString{Value: "test"}}},
+			expected: []interface{}{"test"},
 		},
 	}
 
 	for _, tt := range cases {
-		t.Run(tt.description, func(t *testing.T) {
+		suite.T().Run(tt.description, func(t *testing.T) {
 			i := tt.factValue.AsInterface()
 
-			assert.Equal(t, i, tt.expected)
+			suite.Equal(i, tt.expected)
 		})
 	}
 }
 
-func TestParseStringToFactValue(t *testing.T) {
+func (suite *FactValueTestSuite) TestParseStringToFactValue() {
 	cases := []struct {
 		description string
 		str         string
-		expected    FactValue
+		expected    entities.FactValue
 	}{
 		{
 			description: "Should parse a string to FactValueInt",
 			str:         "1",
-			expected:    &FactValueInt{Value: 1},
+			expected:    &entities.FactValueInt{Value: 1},
 		},
 		{
 			description: "Should parse a string to  FactValueFloat",
 			str:         "1.1",
-			expected:    &FactValueFloat{Value: 1.1},
+			expected:    &entities.FactValueFloat{Value: 1.1},
 		},
 
 		{
 			description: "Should parse a string to FactValueBool",
 			str:         "true",
-			expected:    &FactValueBool{Value: true},
+			expected:    &entities.FactValueBool{Value: true},
 		},
 		{
 			description: "Should parse a string to FactValueString",
 			str:         "test",
-			expected:    &FactValueString{Value: "test"},
+			expected:    &entities.FactValueString{Value: "test"},
 		},
 	}
 
 	for _, tt := range cases {
-		t.Run(tt.description, func(t *testing.T) {
-			factValue := ParseStringToFactValue(tt.str)
+		suite.T().Run(tt.description, func(t *testing.T) {
+			factValue := entities.ParseStringToFactValue(tt.str)
 
-			assert.Equal(t, factValue, tt.expected)
+			suite.Equal(factValue, tt.expected)
 		})
 	}
 }
