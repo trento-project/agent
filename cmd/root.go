378ad540d3a6fc756c4f51da32442d06245c441b
Nelson Kopliku <nelson.kopliku@suse.com>
2022-12-14 09:52:27 +0100

    Add a new command `trento-agent id` that returns the agent's identifier

--- cmd/root.go
+++ cmd/root.go
@@ -39,6 +39,7 @@ that can help you deploy, provision and operate infrastructure for SAP Applicati
 		}
 	})
 
+	rootCmd.AddCommand(NewAgentIDCmd())
 	rootCmd.AddCommand(NewStartCmd())
 	rootCmd.AddCommand(NewFactsCmd())
 	rootCmd.AddCommand(NewVersionCmd())
