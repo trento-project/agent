0e0e87dcc5a2f519ac348cd9d8235e6f7d6eaf35
arbulu89 <xarbulu@suse.com>
2022-12-23 09:54:25 +0100

    Include brackets and comments to improve readability

--- internal/factsengine/gatherers/xml.go
+++ internal/factsengine/gatherers/xml.go
@@ -32,26 +32,38 @@ func parseXMLToFactValueMap(xmlContent []byte, elementsToList map[string]bool) (
 	return factValueMap, nil
 }
 
+// convertListElements returns and updated version of the current xml map, where the keys in elementsToList
+// are converted to lists if they are unique entries. This is required due how xml works, as the initial
+// xml to map conversion cannot know if the coming entry is an unique element or not
 func convertListElements(currentMap map[string]interface{}, elementsToList map[string]bool) map[string]interface{} {
 	convertedMap := make(map[string]interface{})
 	for key, value := range currentMap {
 		switch assertedValue := value.(type) {
 		case map[string]interface{}:
-			convertedMap[key] = convertListElements(assertedValue, elementsToList)
-		case []interface{}:
-			newList := []interface{}{}
-			for _, item := range assertedValue {
-				if toMap, ok := item.(map[string]interface{}); ok {
-					newList = append(newList, convertListElements(toMap, elementsToList))
-				} else {
-					newList = append(newList, item)
-				}
+			{
+				convertedMap[key] = convertListElements(assertedValue, elementsToList)
 			}
-			convertedMap[key] = newList
+		// Item is a list, so each element must be treated to see if its children need a conversion.
+		// The items could be maps itself, so they must recurse yet again
+		case []interface{}:
+			{
+				newList := []interface{}{}
+				for _, item := range assertedValue {
+					if toMap, ok := item.(map[string]interface{}); ok {
+						newList = append(newList, convertListElements(toMap, elementsToList))
+					} else {
+						newList = append(newList, item)
+					}
+				}
+				convertedMap[key] = newList
+			}
 		default:
-			convertedMap[key] = assertedValue
+			{
+				convertedMap[key] = assertedValue
+			}
 		}
 
+		// If the current key is not a list and it is included in the elementsToList, initialize as list
 		_, isList := convertedMap[key].([]interface{})
 		if elementsToList[key] && !isList {
 			convertedMap[key] = []interface{}{convertedMap[key]}
