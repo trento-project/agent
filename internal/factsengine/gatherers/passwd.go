083f3168565cd9c15bb6cd23ed5720491834b5c6
Xabier Arbulu Insausti <xarbulu@suse.com>
2023-09-27 13:44:03 +0200

    Add passwd file gatherer (#261)
    * Add passwd file gatherer
    
    * Rename internal function names
    
    * Add the new gatherer in the gatherers registry
    
    * Fix typo in logging
    
    * Add StringConversion to the fact value creation
    
    * Rename Entry to PasswdEntry

---
+++ internal/factsengine/gatherers/passwd.go
@@ -0,0 +1,137 @@
+package gatherers
+
+import (
+	"bufio"
+	"encoding/json"
+	"fmt"
+	"os"
+	"strings"
+
+	log "github.com/sirupsen/logrus"
+	"github.com/trento-project/agent/pkg/factsengine/entities"
+)
+
+const (
+	PasswdGathererName = "passwd"
+	PasswdFilePath     = "/etc/passwd"
+)
+
+// nolint:gochecknoglobals
+var (
+	PasswdFileError = entities.FactGatheringError{
+		Type:    "passwd-file-error",
+		Message: "error reading /etc/passwd file",
+	}
+
+	PasswdDecodingError = entities.FactGatheringError{
+		Type:    "passwd-decoding-error",
+		Message: "error decoding file content",
+	}
+)
+
+// A PasswdEntry contains all the fields for a specific user
+type PasswdEntry struct {
+	User        string `json:"user"`
+	UID         string `json:"uid"`
+	GID         string `json:"gid"`
+	Description string `json:"description"`
+	Home        string `json:"home"`
+	Shell       string `json:"shell"`
+}
+
+type PasswdGatherer struct {
+	passwdFilePath string
+}
+
+func NewDefaultPasswdGatherer() *PasswdGatherer {
+	return NewPasswdGatherer(PasswdFilePath)
+}
+
+func NewPasswdGatherer(path string) *PasswdGatherer {
+	return &PasswdGatherer{
+		passwdFilePath: path,
+	}
+}
+
+func (g *PasswdGatherer) Gather(factsRequests []entities.FactRequest) ([]entities.Fact, error) {
+	facts := []entities.Fact{}
+	log.Infof("Starting %s facts gathering process", PasswdGathererName)
+
+	entries, err := parsePasswdFile(g.passwdFilePath)
+	if err != nil {
+		return nil, PasswdFileError.Wrap(err.Error())
+	}
+
+	factValues, err := convertEntriesToFactValue(entries)
+	if err != nil {
+		return nil, PasswdDecodingError.Wrap(err.Error())
+	}
+
+	for _, requestedFact := range factsRequests {
+		fact := entities.NewFactGatheredWithRequest(requestedFact, factValues)
+
+		facts = append(facts, fact)
+	}
+
+	log.Infof("Requested %s facts gathered", PasswdGathererName)
+	return facts, nil
+}
+
+func parsePasswdFile(filePath string) ([]PasswdEntry, error) {
+	entries := []PasswdEntry{}
+
+	passwdFile, err := os.Open(filePath)
+	if err != nil {
+		return nil, err
+	}
+
+	defer func() {
+		err := passwdFile.Close()
+		if err != nil {
+			log.Error(err)
+		}
+	}()
+
+	fileScanner := bufio.NewScanner(passwdFile)
+	fileScanner.Split(bufio.ScanLines)
+	var fileLines []string
+
+	for fileScanner.Scan() {
+		scannedLine := fileScanner.Text()
+		fileLines = append(fileLines, scannedLine)
+	}
+
+	for index, line := range fileLines {
+		values := strings.Split(line, ":")
+		if len(values) != 7 {
+			return nil, fmt.Errorf("invalid passwd file: line %d entry does not have 7 values", index+1)
+		}
+		newEntry := PasswdEntry{
+			User:        values[0],
+			UID:         values[2],
+			GID:         values[3],
+			Description: values[4],
+			Home:        values[5],
+			Shell:       values[6],
+		}
+
+		entries = append(entries, newEntry)
+	}
+
+	return entries, nil
+}
+
+func convertEntriesToFactValue(entries []PasswdEntry) (entities.FactValue, error) {
+	marshalled, err := json.Marshal(&entries)
+	if err != nil {
+		return nil, err
+	}
+
+	var unmarshalled []interface{}
+	err = json.Unmarshal(marshalled, &unmarshalled)
+	if err != nil {
+		return nil, err
+	}
+
+	return entities.NewFactValue(unmarshalled, entities.WithStringConversion())
+}
---
+++ internal/factsengine/gatherers/passwd_test.go
@@ -0,0 +1,109 @@
+package gatherers_test
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/suite"
+	"github.com/trento-project/agent/internal/factsengine/gatherers"
+	"github.com/trento-project/agent/pkg/factsengine/entities"
+	"github.com/trento-project/agent/test/helpers"
+)
+
+type PasswdTestSuite struct {
+	suite.Suite
+}
+
+func TestPasswdTestSuite(t *testing.T) {
+	suite.Run(t, new(PasswdTestSuite))
+}
+
+func (suite *PasswdTestSuite) TestPasswd() {
+	c := gatherers.NewPasswdGatherer(helpers.GetFixturePath("gatherers/passwd.basic"))
+
+	factRequests := []entities.FactRequest{
+		{
+			Name:     "passwd",
+			Gatherer: "passwd",
+			CheckID:  "check1",
+		},
+	}
+
+	factResults, err := c.Gather(factRequests)
+
+	expectedResults := []entities.Fact{
+		{
+			Name: "passwd",
+			Value: &entities.FactValueList{Value: []entities.FactValue{
+				&entities.FactValueMap{Value: map[string]entities.FactValue{
+					"user":        &entities.FactValueString{Value: "bin"},
+					"uid":         &entities.FactValueInt{Value: 1},
+					"gid":         &entities.FactValueInt{Value: 1},
+					"description": &entities.FactValueString{Value: "bin"},
+					"home":        &entities.FactValueString{Value: "/bin"},
+					"shell":       &entities.FactValueString{Value: "/sbin/nologin"},
+				}},
+				&entities.FactValueMap{Value: map[string]entities.FactValue{
+					"user":        &entities.FactValueString{Value: "chrony"},
+					"uid":         &entities.FactValueInt{Value: 474},
+					"gid":         &entities.FactValueInt{Value: 475},
+					"description": &entities.FactValueString{Value: "Chrony Daemon"},
+					"home":        &entities.FactValueString{Value: "/var/lib/chrony"},
+					"shell":       &entities.FactValueString{Value: "/bin/false"},
+				}},
+				&entities.FactValueMap{Value: map[string]entities.FactValue{
+					"user":        &entities.FactValueString{Value: "pgadmin"},
+					"uid":         &entities.FactValueInt{Value: 441},
+					"gid":         &entities.FactValueInt{Value: 440},
+					"description": &entities.FactValueString{Value: "pgadmin4"},
+					"home":        &entities.FactValueString{Value: "/var/lib/pgadmin"},
+					"shell":       &entities.FactValueString{Value: "/bin/false"},
+				}},
+				&entities.FactValueMap{Value: map[string]entities.FactValue{
+					"user":        &entities.FactValueString{Value: "uuidd"},
+					"uid":         &entities.FactValueInt{Value: 440},
+					"gid":         &entities.FactValueInt{Value: 439},
+					"description": &entities.FactValueString{Value: "User for uuidd"},
+					"home":        &entities.FactValueString{Value: "/var/run/uuidd"},
+					"shell":       &entities.FactValueString{Value: "/sbin/nologin"},
+				}},
+			}},
+			CheckID: "check1",
+		},
+	}
+
+	suite.NoError(err)
+	suite.ElementsMatch(expectedResults, factResults)
+}
+
+func (suite *PasswdTestSuite) TestPasswdFileNotExists() {
+	c := gatherers.NewPasswdGatherer("non_existing_file")
+
+	factRequests := []entities.FactRequest{
+		{
+			Name:     "passwd",
+			Gatherer: "passwd",
+		},
+	}
+
+	_, err := c.Gather(factRequests)
+
+	suite.EqualError(err, "fact gathering error: passwd-file-error - error reading /etc/passwd file: "+
+		"open non_existing_file: no such file or directory")
+}
+
+func (suite *PasswdTestSuite) TestPasswdErrorDecoding() {
+
+	c := gatherers.NewPasswdGatherer(helpers.GetFixturePath("gatherers/passwd.invalid"))
+
+	factRequests := []entities.FactRequest{
+		{
+			Name:     "passwd",
+			Gatherer: "passwd",
+		},
+	}
+
+	_, err := c.Gather(factRequests)
+
+	suite.EqualError(err, "fact gathering error: passwd-file-error - error reading /etc/passwd file: "+
+		"invalid passwd file: line 1 entry does not have 7 values")
+}
