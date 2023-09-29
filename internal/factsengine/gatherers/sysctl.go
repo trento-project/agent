ac12d77daabdbb8b8959d97a8761b87d9d85fe80
Rubén Torrero Marijnissen <rtorreromarijnissen@suse.com>
2023-09-29 15:27:03 +0100

    Add sysctl gatherer

---
+++ internal/factsengine/gatherers/sysctl.go
@@ -0,0 +1,120 @@
+package gatherers
+
+import (
+	"strings"
+
+	log "github.com/sirupsen/logrus"
+	"github.com/trento-project/agent/pkg/factsengine/entities"
+	"github.com/trento-project/agent/pkg/utils"
+)
+
+const (
+	SysctlGathererName = "sysctl"
+)
+
+// nolint:gochecknoglobals
+var (
+	SysctlValueNotFound = entities.FactGatheringError{
+		Type:    "sysctl-value-not-found",
+		Message: "requested value not found in sysctl output",
+	}
+
+	SysctlCommandError = entities.FactGatheringError{
+		Type:    "sysctl-cmd-error",
+		Message: "error executing sysctl command",
+	}
+
+	SysctlMissingArgument = entities.FactGatheringError{
+		Type:    "sysctl-missing-argument",
+		Message: "missing required argument",
+	}
+)
+
+type SysctlGatherer struct {
+	executor utils.CommandExecutor
+}
+
+func NewDefaultSysctlGatherer() *SysctlGatherer {
+	return NewSysctlGatherer(utils.Executor{})
+}
+
+func NewSysctlGatherer(executor utils.CommandExecutor) *SysctlGatherer {
+	return &SysctlGatherer{
+		executor: executor,
+	}
+}
+
+func (s *SysctlGatherer) Gather(factsRequests []entities.FactRequest) ([]entities.Fact, error) {
+	facts := []entities.Fact{}
+	log.Infof("Starting sysctl facts gathering process")
+
+	output, err := s.executor.Exec("sysctl", "-a")
+	if err != nil {
+		return nil, SysctlCommandError.Wrap(err.Error())
+	}
+
+	sysctlMap := sysctlOutputToMap(output)
+	for _, factReq := range factsRequests {
+		var fact entities.Fact
+
+		if len(factReq.Argument) == 0 {
+			log.Error(SysctlMissingArgument.Message)
+			fact = entities.NewFactGatheredWithError(factReq, &SysctlMissingArgument)
+		} else if value, err := sysctlMap.GetValue(factReq.Argument); err == nil {
+			fact = entities.NewFactGatheredWithRequest(factReq, value)
+		} else {
+			gatheringError := SysctlValueNotFound.Wrap(factReq.Argument)
+			log.Error(gatheringError)
+			fact = entities.NewFactGatheredWithError(factReq, gatheringError)
+		}
+
+		facts = append(facts, fact)
+	}
+
+	log.Infof("Requested %s facts gathered", CorosyncCmapCtlGathererName)
+	return facts, nil
+}
+
+func sysctlOutputToMap(output []byte) *entities.FactValueMap {
+	outputMap := &entities.FactValueMap{Value: make(map[string]entities.FactValue)}
+	var cursor *entities.FactValueMap
+
+	for _, line := range strings.Split(string(output), "\n") {
+		if len(line) == 0 {
+			continue
+		}
+
+		parts := strings.SplitN(strings.TrimSpace(line), " = ", 2)
+		switch {
+		case len(parts) == 1:
+			if strings.HasSuffix(parts[0], " =") {
+				parts[0] = parts[0][:len(parts[0])-2]
+			}
+			parts = append(parts, "")
+		case len(parts) == 2:
+		default:
+			continue
+		}
+
+		value := parts[1]
+		path := strings.Split(parts[0], ".")
+		cursor = outputMap
+
+		for i, key := range path {
+			currentMap, ok := cursor.Value[key].(*entities.FactValueMap)
+			if !ok {
+				if i == len(path)-1 {
+					cursor.Value[key] = entities.ParseStringToFactValue(value)
+					break
+				} else {
+					currentMap = &entities.FactValueMap{Value: make(map[string]entities.FactValue)}
+					cursor.Value[key] = currentMap
+				}
+			}
+
+			cursor = currentMap
+		}
+	}
+
+	return outputMap
+}
---
+++ internal/factsengine/gatherers/sysctl_test.go
@@ -0,0 +1,184 @@
+package gatherers_test
+
+import (
+	"io"
+	"os"
+	"os/exec"
+	"testing"
+
+	"github.com/stretchr/testify/suite"
+	"github.com/trento-project/agent/internal/factsengine/gatherers"
+	"github.com/trento-project/agent/pkg/factsengine/entities"
+	utilsMocks "github.com/trento-project/agent/pkg/utils/mocks"
+	"github.com/trento-project/agent/test/helpers"
+)
+
+type SysctlTestSuite struct {
+	suite.Suite
+	mockExecutor *utilsMocks.CommandExecutor
+}
+
+func TestSysctlTestSuite(t *testing.T) {
+	suite.Run(t, new(SysctlTestSuite))
+}
+
+func (suite *SysctlTestSuite) SetupTest() {
+	suite.mockExecutor = new(utilsMocks.CommandExecutor)
+}
+
+func (suite *SysctlTestSuite) TestSysctlGathererNoArgumentProvided() {
+	mockOutputFile, _ := os.Open(helpers.GetFixturePath("gatherers/sysctl.output"))
+	mockOutput, _ := io.ReadAll(mockOutputFile)
+	suite.mockExecutor.On("Exec", "sysctl", "-a").Return(mockOutput, nil)
+
+	c := gatherers.NewSysctlGatherer(suite.mockExecutor)
+
+	factRequests := []entities.FactRequest{
+		{
+			Name:     "no_argument_fact",
+			Gatherer: "sysctl",
+		},
+		{
+			Name:     "empty_argument_fact",
+			Gatherer: "sysctl",
+			Argument: "",
+		},
+	}
+
+	factResults, err := c.Gather(factRequests)
+
+	expectedResults := []entities.Fact{
+		{
+			Name:  "no_argument_fact",
+			Value: nil,
+			Error: &entities.FactGatheringError{
+				Message: "missing required argument",
+				Type:    "sysctl-missing-argument",
+			},
+		},
+		{
+			Name:  "empty_argument_fact",
+			Value: nil,
+			Error: &entities.FactGatheringError{
+				Message: "missing required argument",
+				Type:    "sysctl-missing-argument",
+			},
+		},
+	}
+
+	suite.NoError(err)
+	suite.ElementsMatch(expectedResults, factResults)
+}
+
+func (suite *SysctlTestSuite) TestSysctlGathererNonExistingKey() {
+	mockOutputFile, _ := os.Open(helpers.GetFixturePath("gatherers/sysctl.output"))
+	mockOutput, _ := io.ReadAll(mockOutputFile)
+	suite.mockExecutor.On("Exec", "sysctl", "-a").Return(mockOutput, nil)
+
+	c := gatherers.NewSysctlGatherer(suite.mockExecutor)
+
+	factRequests := []entities.FactRequest{
+		{
+			Name:     "madeup_fact",
+			Gatherer: "sysctl",
+			Argument: "madeup.fact",
+		},
+	}
+
+	factResults, err := c.Gather(factRequests)
+
+	expectedResults := []entities.Fact{
+		{
+			Name:  "madeup_fact",
+			Value: nil,
+			Error: &entities.FactGatheringError{
+				Message: "requested value not found in sysctl output: madeup.fact",
+				Type:    "sysctl-value-not-found",
+			},
+		},
+	}
+
+	suite.NoError(err)
+	suite.ElementsMatch(expectedResults, factResults)
+}
+
+func (suite *SysctlTestSuite) TestSysctlCommandNotFound() {
+	suite.mockExecutor.On("Exec", "sysctl", "-a").Return(nil, exec.ErrNotFound)
+
+	c := gatherers.NewSysctlGatherer(suite.mockExecutor)
+
+	factRequests := []entities.FactRequest{
+		{
+			Name:     "fs.inotify.max_user_watches",
+			Gatherer: "sysctl",
+			Argument: "fs.inotify.max_user_watches",
+		},
+	}
+
+	expectedError := &entities.FactGatheringError{
+		Message: "error executing sysctl command: executable file not found in $PATH",
+		Type:    "sysctl-cmd-error",
+	}
+
+	factResults, err := c.Gather(factRequests)
+
+	suite.EqualError(err, expectedError.Error())
+
+	suite.Empty(factResults)
+}
+
+func (suite *SysctlTestSuite) TestSysctlGatherer() {
+	mockOutputFile, _ := os.Open(helpers.GetFixturePath("gatherers/sysctl.output"))
+	mockOutput, _ := io.ReadAll(mockOutputFile)
+	suite.mockExecutor.On("Exec", "sysctl", "-a").Return(mockOutput, nil)
+
+	c := gatherers.NewSysctlGatherer(suite.mockExecutor)
+
+	factRequests := []entities.FactRequest{
+		{
+			Name:     "simple_value",
+			Gatherer: "sysctl",
+			Argument: "fs.inotify.max_user_watches",
+		},
+	}
+
+	factResults, err := c.Gather(factRequests)
+
+	expectedResults := []entities.Fact{
+		{
+			Name:  "simple_value",
+			Value: &entities.FactValueInt{Value: 65536},
+		},
+	}
+
+	suite.NoError(err)
+	suite.ElementsMatch(expectedResults, factResults)
+}
+
+func (suite *SysctlTestSuite) TestSysctlGathererEmptyValue() {
+	mockOutputFile, _ := os.Open(helpers.GetFixturePath("gatherers/sysctl.output"))
+	mockOutput, _ := io.ReadAll(mockOutputFile)
+	suite.mockExecutor.On("Exec", "sysctl", "-a").Return(mockOutput, nil)
+
+	c := gatherers.NewSysctlGatherer(suite.mockExecutor)
+
+	factRequests := []entities.FactRequest{
+		{
+			Name:     "empty_value",
+			Gatherer: "sysctl",
+			Argument: "kernel.domainname",
+		},
+	}
+
+	factResults, err := c.Gather(factRequests)
+
+	expectedResults := []entities.Fact{
+		{
+			Name:  "empty_value",
+			Value: &entities.FactValueString{Value: ""}, // We should probably use the upcoming FactValueNil (PR#263)
+		},
+	}
+
+	suite.NoError(err)
+	suite.ElementsMatch(expectedResults, factResults)
+}
