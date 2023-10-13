c939cb7f63fb02faa8ac70ad0e8c7c3ea6bbee60
Carmine Di Monaco <carmine.dimonaco@gmail.com>
2023-10-13 10:28:33 +0200

    Refactored gatherers registry to support gatherer versioning

--- internal/factsengine/gatherers/registry.go
+++ internal/factsengine/gatherers/registry.go
@@ -1,32 +1,72 @@
 package gatherers
 
-import "github.com/pkg/errors"
+import (
+	"fmt"
+	"sort"
+	"strings"
+)
+
+type GathererNotFoundError struct {
+	Name string
+}
+
+func (e *GathererNotFoundError) Error() string {
+	return fmt.Sprintf("gatherer %s not found", e.Name)
+}
+
+// map[gathererName]map[GathererVersion]Gatherer
+type Tree map[string]map[string]FactGatherer
 
 type Registry struct {
-	gatherers map[string]FactGatherer
+	gatherers Tree
+}
+
+func NewRegistry(gatherers Tree) *Registry {
+	return &Registry{
+		gatherers: gatherers,
+	}
 }
 
 func (m *Registry) GetGatherer(name string) (FactGatherer, error) {
-	if g, found := m.gatherers[name]; found {
+	gathererName, version, err := extractVersionAndGathererName(name)
+	if err != nil {
+		return nil, err
+	}
+	if version == "" {
+		latestVersion, err := m.getLatestVersionForGatherer(name)
+		if err != nil {
+			return nil, err
+		}
+		version = latestVersion
+	}
+
+	if g, found := m.gatherers[gathererName][version]; found {
 		return g, nil
 	}
-	return nil, errors.Errorf("gatherer %s not found", name)
+	return nil, &GathererNotFoundError{Name: name}
 }
 
 func (m *Registry) AvailableGatherers() []string {
 	gatherersList := []string{}
 
-	for gatherer := range m.gatherers {
-		gatherersList = append(gatherersList, gatherer)
+	for gatherer, versions := range m.gatherers {
+		gathererVersions := []string{}
+		for v := range versions {
+			gathererVersions = append(gathererVersions, v)
+		}
+		gatherersList = append(
+			gatherersList,
+			fmt.Sprintf("%s - %s", gatherer, strings.Join(gathererVersions, "/")),
+		)
 	}
 
 	return gatherersList
 }
 
 // This is not safe, please not use concurrently.
-func (m *Registry) AddGatherers(gatherers map[string]FactGatherer) {
-	maps := []map[string]FactGatherer{m.gatherers, gatherers}
-	result := make(map[string]FactGatherer)
+func (m *Registry) AddGatherers(gatherers Tree) {
+	maps := []Tree{m.gatherers, gatherers}
+	result := make(Tree)
 
 	for _, m := range maps {
 		for k, v := range m {
@@ -36,8 +76,32 @@ func (m *Registry) AddGatherers(gatherers map[string]FactGatherer) {
 	m.gatherers = result
 }
 
-func NewRegistry(gatherers map[string]FactGatherer) *Registry {
-	return &Registry{
-		gatherers: gatherers,
+func (m *Registry) getLatestVersionForGatherer(name string) (string, error) {
+	availableGatherers, found := m.gatherers[name]
+	if !found {
+		return "", &GathererNotFoundError{Name: name}
 	}
+	versions := []string{}
+	for v := range availableGatherers {
+		versions = append(versions, v)
+	}
+
+	sort.Strings(versions)
+
+	return versions[len(versions)-1], nil
+}
+
+func extractVersionAndGathererName(gathererName string) (string, string, error) {
+	parts := strings.Split(gathererName, "@")
+	if len(parts) == 1 {
+		// no version found, just gatherer name
+		return parts[0], "", nil
+	}
+	if len(parts) != 2 {
+		return "", "", fmt.Errorf(
+			"could not extract the gatherer version from %s, version should follow <gathererName>@<version> syntax",
+			gathererName,
+		)
+	}
+	return parts[0], parts[1], nil
 }
