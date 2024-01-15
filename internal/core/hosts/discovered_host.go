f57ed8dc00b1b1e2153ec30c082d5cd0a3ac0d10
Alessio Biancalana <alessio.biancalana@suse.com>
2024-01-15 08:57:50 +0100

    Update tests

--- internal/core/hosts/discovered_host.go
+++ internal/core/hosts/discovered_host.go
@@ -1,12 +1,13 @@
 package hosts
 
 type DiscoveredHost struct {
-	OSVersion          string   `json:"os_version"`
-	HostIPAddresses    []string `json:"ip_addresses"`
-	HostName           string   `json:"hostname"`
-	CPUCount           int      `json:"cpu_count"`
-	SocketCount        int      `json:"socket_count"`
-	TotalMemoryMB      int      `json:"total_memory_mb"`
-	AgentVersion       string   `json:"agent_version"`
-	InstallationSource string   `json:"installation_source"`
+	OSVersion                string   `json:"os_version"`
+	HostIPAddresses          []string `json:"ip_addresses"`
+	HostName                 string   `json:"hostname"`
+	CPUCount                 int      `json:"cpu_count"`
+	SocketCount              int      `json:"socket_count"`
+	TotalMemoryMB            int      `json:"total_memory_mb"`
+	AgentVersion             string   `json:"agent_version"`
+	InstallationSource       string   `json:"installation_source"`
+	FullyQualifiedDomainName *string  `json:"fully_qualified_domain_name, omitempty"`
 }
--- internal/discovery/mocks/discovered_host_mock.go
+++ internal/discovery/mocks/discovered_host_mock.go
@@ -3,14 +3,17 @@ package mocks
 import "github.com/trento-project/agent/internal/core/hosts"
 
 func NewDiscoveredHostMock() hosts.DiscoveredHost {
+	fqdn := "com.example.trento.host"
+
 	return hosts.DiscoveredHost{
-		OSVersion:          "15-SP2",
-		HostIPAddresses:    []string{"10.1.1.4", "10.1.1.5", "10.1.1.6"},
-		HostName:           "thehostnamewherethediscoveryhappened",
-		CPUCount:           2,
-		SocketCount:        1,
-		TotalMemoryMB:      4096,
-		AgentVersion:       "trento-agent-version",
-		InstallationSource: "Community",
+		OSVersion:                "15-SP2",
+		HostIPAddresses:          []string{"10.1.1.4", "10.1.1.5", "10.1.1.6"},
+		HostName:                 "thehostnamewherethediscoveryhappened",
+		CPUCount:                 2,
+		SocketCount:              1,
+		TotalMemoryMB:            4096,
+		AgentVersion:             "trento-agent-version",
+		InstallationSource:       "Community",
+		FullyQualifiedDomainName: &fqdn,
 	}
 }
