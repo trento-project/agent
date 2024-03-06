ac6d75a01354d7f84209474fedb4b4481b30ea6e
Rubén Torrero Marijnissen <rtorreromarijnissen@suse.com>
2024-03-06 11:26:30 +0000

    Add test to check for error on invalid amqp url

--- internal/agent/agent_test.go
+++ internal/agent/agent_test.go
@@ -1,10 +1,13 @@
 package agent_test
 
 import (
+	"context"
 	"testing"
 
 	"github.com/stretchr/testify/suite"
 	"github.com/trento-project/agent/internal/agent"
+	"github.com/trento-project/agent/internal/discovery"
+	"github.com/trento-project/agent/internal/discovery/collector"
 	"github.com/trento-project/agent/test/helpers"
 )
 
@@ -22,4 +25,23 @@ func (suite *AgentTestSuite) TestAgentGetAgentID() {
 
 	suite.NoError(err)
 	suite.Equal(helpers.DummyAgentID, agentID)
+}
+
+func (suite *AgentTestSuite) TestAgentFailsWithInvalidFactsServiceURL() {
+	config := &agent.Config{
+		AgentID:      helpers.DummyAgentID,
+		InstanceName: "test",
+		DiscoveriesConfig: &discovery.DiscoveriesConfig{
+			DiscoveriesPeriodsConfig: &discovery.DiscoveriesPeriodConfig{},
+			CollectorConfig:          &collector.Config{},
+		},
+		FactsServiceURL: "amqp://trento:trento@somehost:1234/somevhost",
+		PluginsFolder:   "/usr/etc/trento/plugins/",
+	}
+
+	agent, _ := agent.NewAgent(config)
+	ctx := context.Background()
+	err := agent.Start(ctx)
+
+	suite.ErrorContains(err, "dial tcp: lookup somehost: no such host")
 }
