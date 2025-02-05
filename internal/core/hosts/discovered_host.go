d69bc8482cf8a92b02f40f89489e77be34be9b55
balanza <emanuele.decupis@suse.com>
2025-02-05 22:44:42 +0100

    fix int overflow on discovery

--- internal/core/hosts/discovered_host.go
+++ internal/core/hosts/discovered_host.go
@@ -7,7 +7,7 @@ type DiscoveredHost struct {
 	HostName                 string            `json:"hostname"`
 	CPUCount                 int               `json:"cpu_count"`
 	SocketCount              int               `json:"socket_count"`
-	TotalMemoryMB            int               `json:"total_memory_mb"`
+	TotalMemoryMB            uint64            `json:"total_memory_mb"`
 	AgentVersion             string            `json:"agent_version"`
 	InstallationSource       string            `json:"installation_source"`
 	FullyQualifiedDomainName *string           `json:"fully_qualified_domain_name,omitempty"`
