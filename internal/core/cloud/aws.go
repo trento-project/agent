206763862403a6f9f93171d283a356059492859b
Nelson Kopliku <nelson.kopliku@suse.com>
2025-03-29 14:57:52 +0100

    Support aws imds v2

--- internal/core/cloud/aws.go
+++ internal/core/cloud/aws.go
@@ -12,13 +12,63 @@ import (
 	"fmt"
 	"io"
 	"net/http"
+	"strconv"
 	"strings"
+
+	"github.com/pkg/errors"
+	log "github.com/sirupsen/logrus"
+	caching "github.com/trento-project/agent/pkg/cache"
 )
 
 const (
-	awsMetadataURL      = "http://169.254.169.254/latest/"
-	awsMetadataResource = "meta-data"
+	awsMetadataURL                = "http://169.254.169.254/latest/"
+	awsMetadataResource           = "meta-data"
+	metadataTokenTTLHeader string = "X-aws-ec2-metadata-token-ttl-seconds" //nolint
+	metadataTokenHeader    string = "X-aws-ec2-metadata-token"             //nolint
+	maxRefreshAttempts     byte   = 3
+
+	// https://docs.aws.amazon.com/AWSEC2/latest/UserGuide/configuring-instance-metadata-service.html
+	// (TTL) for the token, in seconds, up to a maximum of six hours (21,600 seconds).
+	awsEC2MetadataTokenTTL     int    = 60 * 60 * 6 // 6 hours
+	metadataFetchTokenCacheKey string = "awsMetadataToken"
 )
+
+type MetadataFetchContext struct {
+	Token          string
+	RefreshAttempt byte
+	URL            string
+	cache          *caching.Cache
+}
+
+func (context MetadataFetchContext) HasToken() bool {
+	return context.Token != ""
+}
+
+func (context MetadataFetchContext) WithToken(token string) MetadataFetchContext {
+	entry := context.cache.Update(metadataFetchTokenCacheKey, caching.DefaultUpdateFn(token))
+	context.Token = entry.Content.(string)
+	return context
+}
+
+func (context MetadataFetchContext) IncreaseTokenRefreshAttempt() MetadataFetchContext {
+	context.cache.Delete(metadataFetchTokenCacheKey)
+	context.Token = ""
+	context.RefreshAttempt++
+	return context
+}
+
+func (context MetadataFetchContext) WithNextURL(part string) MetadataFetchContext {
+	context.URL += part
+	return context
+}
+
+func NewMetadataFetchContext(cache *caching.Cache) MetadataFetchContext {
+	return MetadataFetchContext{
+		URL:   awsMetadataURL,
+		Token: cache.GetOrDefault(metadataFetchTokenCacheKey, "").Content.(string),
+		cache: cache,
+	}
+}
 
 type AWSMetadata struct {
 	AmiID               string              `json:"ami-id"`
@@ -53,7 +103,15 @@ type Placement struct {
 	Region           string `json:"region"`
 }
 
-func NewAWSMetadata(ctx context.Context, client HTTPClient) (*AWSMetadata, error) {
+func NewAWSMetadata(ctx context.Context, client HTTPClient, cache *caching.Cache) (*AWSMetadata, error) {
+	return NewAWSMetadataWithFetchContext(ctx, client, NewMetadataFetchContext(cache))
+}
+
+func NewAWSMetadataWithFetchContext(
+	ctx context.Context,
+	client HTTPClient,
+	metadataFetchContext MetadataFetchContext,
+) (*AWSMetadata, error) {
 	var err error
 	awsMetadata := &AWSMetadata{
 		AmiID:              "",
@@ -87,7 +145,7 @@ func NewAWSMetadata(ctx context.Context, client HTTPClient) (*AWSMetadata, error
 	}
 
 	firstElementsList := []string{fmt.Sprintf("%s/", awsMetadataResource)}
-	metadata, err := buildAWSMetadata(ctx, client, awsMetadataURL, firstElementsList)
+	metadata, err := buildAWSMetadata(ctx, client, firstElementsList, metadataFetchContext)
 	if err != nil {
 		return nil, err
 	}
@@ -108,15 +166,18 @@ func NewAWSMetadata(ctx context.Context, client HTTPClient) (*AWSMetadata, error
 func buildAWSMetadata(
 	ctx context.Context,
 	client HTTPClient,
-	url string,
 	elements []string,
-) (map[string]interface{}, error) {
-	metadata := make(map[string]interface{})
+	metadataFetchContext MetadataFetchContext,
+) (map[string]any, error) {
+	metadata := make(map[string]any)
+
+	metadataFetchContext, err := requestMetadataToken(ctx, client, metadataFetchContext)
+	if err != nil {
+		return nil, err
+	}
 
 	for _, element := range elements {
-		newURL := url + element
-
-		response, err := requestMetadata(ctx, client, newURL)
+		response, metadataFetchContext, err := requestMetadata(ctx, client, metadataFetchContext.WithNextURL(element))
 		if err != nil {
 			return metadata, err
 		}
@@ -125,7 +186,7 @@ func buildAWSMetadata(
 			currentElement := strings.Trim(element, "/")
 			newElements := strings.Split(fmt.Sprintf("%v", response), "\n")
 
-			metadata[currentElement], err = buildAWSMetadata(ctx, client, newURL, newElements)
+			metadata[currentElement], err = buildAWSMetadata(ctx, client, newElements, metadataFetchContext)
 			if err != nil {
 				return nil, err
 			}
@@ -137,25 +198,109 @@ func buildAWSMetadata(
 	return metadata, nil
 }
 
-func requestMetadata(ctx context.Context, client HTTPClient, url string) (interface{}, error) {
-	req, _ := http.NewRequestWithContext(ctx, http.MethodGet, url, nil)
+func requestMetadata(
+	ctx context.Context,
+	client HTTPClient,
+	metadataFetchContext MetadataFetchContext,
+) (any, MetadataFetchContext, error) {
+	req, _ := http.NewRequestWithContext(ctx, http.MethodGet, metadataFetchContext.URL, nil)
+	req.Header.Add(metadataTokenHeader, metadataFetchContext.Token)
 
 	resp, err := client.Do(req)
 	if err != nil {
-		return nil, err
+		return nil, metadataFetchContext, err
+	}
+
+	if resp.StatusCode == http.StatusUnauthorized {
+		metadataFetchContext, err := refreshToken(ctx, client, metadataFetchContext)
+		if err != nil {
+			return nil, metadataFetchContext, err
+		}
+
+		return requestMetadata(ctx, client, metadataFetchContext)
+	}
+
+	if resp.StatusCode == http.StatusForbidden {
+		return nil, metadataFetchContext, errors.Errorf("failed to fetch metadata. IMDS might be disabled: %s", resp.Status)
 	}
 
 	defer resp.Body.Close()
 	body, err := io.ReadAll(resp.Body)
 	if err != nil {
-		return nil, err
+		return nil, metadataFetchContext, err
 	}
 
 	// The metadata endpoint may return json elements
 	if json.Valid(body) {
-		var jsonData interface{}
+		var jsonData any
 		err := json.Unmarshal(body, &jsonData)
-		return jsonData, err
+		return jsonData, metadataFetchContext, err
 	}
-	return string(body), nil
+	return string(body), metadataFetchContext, nil
+}
+
+func refreshToken(
+	ctx context.Context,
+	client HTTPClient,
+	metadataFetchContext MetadataFetchContext,
+) (MetadataFetchContext, error) {
+	metadataFetchContext = metadataFetchContext.IncreaseTokenRefreshAttempt()
+	log.Debugf("Refreshing metadata token... attempt %d", metadataFetchContext.RefreshAttempt)
+
+	if metadataFetchContext.RefreshAttempt > maxRefreshAttempts {
+		return metadataFetchContext, errors.Errorf("failed to fetch metadata: reached maximum token refresh attempts")
+	}
+
+	metadataFetchContext, err := requestMetadataToken(
+		ctx,
+		client,
+		metadataFetchContext,
+	)
+
+	if err != nil {
+		log.Debugf("failed to refresh metadata token: %s", err)
+		return refreshToken(ctx, client, metadataFetchContext)
+	}
+
+	log.Debug("Metadata token refreshed successfully")
+	return metadataFetchContext, nil
+}
+
+func requestMetadataToken(
+	ctx context.Context,
+	client HTTPClient,
+	metadataFetchContext MetadataFetchContext,
+) (MetadataFetchContext, error) {
+	log.Debug("Fetching IMDS token...")
+
+	if metadataFetchContext.HasToken() {
+		log.Debug("Using previously fetched IMDS token")
+		return metadataFetchContext, nil
+	}
+
+	url := fmt.Sprintf("%sapi/token", awsMetadataURL)
+
+	req, _ := http.NewRequestWithContext(ctx, http.MethodPut, url, nil)
+
+	req.Header.Add(metadataTokenTTLHeader, strconv.Itoa(awsEC2MetadataTokenTTL))
+
+	resp, err := client.Do(req)
+	if err != nil {
+		log.Debugf("An error occurred while fetching IMDS token: %s", err)
+		return metadataFetchContext, err
+	}
+
+	if resp.StatusCode != http.StatusOK {
+		return metadataFetchContext, errors.Errorf("failed to fetch metadata token: %s", resp.Status)
+	}
+
+	defer resp.Body.Close()
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return metadataFetchContext, err
+	}
+
+	log.Debug("Metadata token fetched successfully")
+
+	return metadataFetchContext.WithToken(string(body)), nil
 }
--- internal/core/cloud/aws_test.go
+++ internal/core/cloud/aws_test.go
@@ -3,6 +3,7 @@ package cloud_test
 import (
 	"bytes"
 	"context"
+	"fmt"
 	"io"
 	"net/http"
 	"os"
@@ -13,47 +14,161 @@ import (
 	"github.com/stretchr/testify/suite"
 	"github.com/trento-project/agent/internal/core/cloud"
 	"github.com/trento-project/agent/internal/core/cloud/mocks"
+	caching "github.com/trento-project/agent/pkg/cache"
 	"github.com/trento-project/agent/test/helpers"
 )
 
 type AWSMetadataTestSuite struct {
 	suite.Suite
+	mockHTTPClient *mocks.HTTPClient
+	cache          *caching.Cache
 }
 
-func TestAWSMetadataTestSuite(t *testing.T) {
-	suite.Run(t, new(AWSMetadataTestSuite))
+type metadataFixtureSpec struct {
+	fixture     string
+	requestPath string
 }
 
-func (suite *AWSMetadataTestSuite) TestNewAWSMetadata() {
-	ctx := context.TODO()
-	clientMock := new(mocks.HTTPClient)
+const mockedIMDSToken string = "awsMetadataToken"
 
-	fixtures := []string{
-		"meta-data",
-		"ami-id",
-		"block-device-mapping",
-		"ebs1",
-		"ebs2",
-		"root",
-		"identity-credentials",
-		"ec2",
-		"info",
-		"instance-id",
-		"instance-type",
-		"network",
-		"interfaces",
-		"macs",
-		"some-mac",
-		"vpc-id",
-		"placement",
-		"availability-zone",
-		"region",
+func matchesMetadataPath(req *http.Request, path string) bool {
+	return req.Method == http.MethodGet && req.URL.Path == path
+}
+
+func matchesMetadataToken(req *http.Request, token string) bool {
+	return req.Header.Get("X-aws-ec2-metadata-token") == token
+}
+
+func fetchTokenRequest() any {
+	return mock.MatchedBy(func(req *http.Request) bool {
+		return req.Method == http.MethodPut &&
+			req.URL.Path == "/latest/api/token" &&
+			req.Header.Get("X-aws-ec2-metadata-token-ttl-seconds") == "21600"
+	})
+}
+
+func matchesRequestPathAndMetadata(matchingPath string, matchingToken string) any {
+	return mock.MatchedBy(func(req *http.Request) bool {
+		return matchesMetadataPath(req, matchingPath) && matchesMetadataToken(req, matchingToken)
+	})
+}
+
+func rootMetadataRequest(matchingToken string) any {
+	return matchesRequestPathAndMetadata("/latest/meta-data/", matchingToken)
+}
+
+func requestWithToken(matchingToken string) any {
+	return mock.MatchedBy(func(req *http.Request) bool {
+		return matchesMetadataToken(req, matchingToken)
+	})
+}
+
+func rootMetadataRequestWithInitialToken() any {
+	return rootMetadataRequest(mockedIMDSToken)
+}
+
+func mockSuccessfulTokenResponse(mockHTTPClient *mocks.HTTPClient, metadataToken string) *mock.Call {
+	token := []byte(metadataToken)
+
+	body := io.NopCloser(bytes.NewReader(token))
+	successfulTokenResponse := &http.Response{
+		StatusCode: 200,
+		Body:       body,
+	}
+
+	return mockHTTPClient.On("Do", fetchTokenRequest()).Return(
+		successfulTokenResponse, nil,
+	)
+}
+
+func mockInitialTokenResponse(mockHTTPClient *mocks.HTTPClient) *mock.Call {
+	return mockSuccessfulTokenResponse(mockHTTPClient, mockedIMDSToken)
+}
+
+func mockSuccessfulMetadataDiscovery(mockHTTPClient *mocks.HTTPClient, matcher func(metadataFixtureSpec) any) {
+	fixtures := []metadataFixtureSpec{
+		{
+			fixture:     "meta-data",
+			requestPath: "/latest/meta-data/",
+		},
+		{
+			fixture:     "ami-id",
+			requestPath: "/latest/meta-data/ami-id",
+		},
+		{
+			fixture:     "block-device-mapping",
+			requestPath: "/latest/meta-data/block-device-mapping/",
+		},
+		{
+			fixture:     "ebs1",
+			requestPath: "/latest/meta-data/block-device-mapping/ebs1",
+		},
+		{
+			fixture:     "ebs2",
+			requestPath: "/latest/meta-data/block-device-mapping/ebs2",
+		},
+		{
+			fixture:     "root",
+			requestPath: "/latest/meta-data/block-device-mapping/root",
+		},
+		{
+			fixture:     "identity-credentials",
+			requestPath: "/latest/meta-data/identity-credentials/",
+		},
+		{
+			fixture:     "ec2",
+			requestPath: "/latest/meta-data/identity-credentials/ec2/",
+		},
+		{
+			fixture:     "info",
+			requestPath: "/latest/meta-data/identity-credentials/ec2/info",
+		},
+		{
+			fixture:     "instance-id",
+			requestPath: "/latest/meta-data/instance-id",
+		},
+		{
+			fixture:     "instance-type",
+			requestPath: "/latest/meta-data/instance-type",
+		},
+		{
+			fixture:     "network",
+			requestPath: "/latest/meta-data/network/",
+		},
+		{
+			fixture:     "interfaces",
+			requestPath: "/latest/meta-data/network/interfaces/",
+		},
+		{
+			fixture:     "macs",
+			requestPath: "/latest/meta-data/network/interfaces/macs/",
+		},
+		{
+			fixture:     "some-mac",
+			requestPath: "/latest/meta-data/network/interfaces/macs/some-mac/",
+		},
+		{
+			fixture:     "vpc-id",
+			requestPath: "/latest/meta-data/network/interfaces/macs/some-mac/vpc-id",
+		},
+		{
+			fixture:     "placement",
+			requestPath: "/latest/meta-data/placement/",
+		},
+		{
+			fixture:     "availability-zone",
+			requestPath: "/latest/meta-data/placement/availability-zone",
+		},
+		{
+			fixture:     "region",
+			requestPath: "/latest/meta-data/placement/region",
+		},
 	}
 
 	fixturesFolder := helpers.GetFixturePath("discovery/aws")
 
-	for _, fixture := range fixtures {
-		aFile, _ := os.Open(path.Join(fixturesFolder, fixture))
+	for _, fixtureSpec := range fixtures {
+		aFile, _ := os.Open(path.Join(fixturesFolder, fixtureSpec.fixture))
 		bodyText, _ := io.ReadAll(aFile)
 		body := io.NopCloser(bytes.NewReader(bodyText))
 
@@ -62,12 +177,168 @@ func (suite *AWSMetadataTestSuite) TestNewAWSMetadata() {
 			Body:       body,
 		}
 
-		clientMock.On("Do", mock.AnythingOfType("*http.Request")).Return(
+		mockHTTPClient.On("Do", matcher(fixtureSpec)).Return(
 			response, nil,
 		).Once()
 	}
+}
 
-	m, err := cloud.NewAWSMetadata(ctx, clientMock)
+func TestAWSMetadataTestSuite(t *testing.T) {
+	suite.Run(t, new(AWSMetadataTestSuite))
+}
+
+func (suite *AWSMetadataTestSuite) SetupTest() {
+	suite.mockHTTPClient = new(mocks.HTTPClient)
+	suite.cache = caching.NewCache()
+}
+
+func withEmptyBody(responses ...*http.Response) []*http.Response {
+	emptyBodies := [][]byte{
+		[]byte(""),
+		[]byte(`
+		`),
+		[]byte(`
+	
+		`),
+	}
+
+	resultingResponses := []*http.Response{}
+	for _, errorResponse := range responses {
+		for _, emptyBody := range emptyBodies {
+			errorResponseWithEmptyBody := errorResponse
+			errorResponseWithEmptyBody.Body = io.NopCloser(bytes.NewReader(emptyBody))
+			resultingResponses = append(resultingResponses, errorResponseWithEmptyBody)
+		}
+	}
+	return resultingResponses
+}
+
+func (suite *AWSMetadataTestSuite) TestUnableToFetchMetadataToken() {
+	responses := []*http.Response{
+		{
+			StatusCode: 400,
+			Status:     "400 Bad Request",
+		},
+		{
+			StatusCode: 401,
+			Status:     "401 Unauthorized",
+		},
+		{
+			StatusCode: 403,
+			Status:     "403 Forbidden",
+		},
+		{
+			StatusCode: 404,
+			Status:     "404 Not Found",
+		},
+		{
+			StatusCode: 500,
+			Status:     "500 Internal Server Error",
+		},
+	}
+
+	for _, response := range withEmptyBody(responses...) {
+		ctx := context.TODO()
+
+		suite.mockHTTPClient.On("Do", fetchTokenRequest()).Return(
+			response, nil,
+		).Once()
+
+		extractedMetadata, err := cloud.NewAWSMetadata(ctx, suite.mockHTTPClient, suite.cache)
+
+		suite.ErrorContains(err, fmt.Sprintf("failed to fetch metadata token: %s", response.Status))
+		suite.Nil(extractedMetadata)
+	}
+
+}
+
+func (suite *AWSMetadataTestSuite) TestUnableToFetchMetadataBecauseDisabledIMDS() {
+	response := &http.Response{
+		StatusCode: 403,
+		Status:     "403 Forbidden",
+	}
+
+	for _, emptyResponse := range withEmptyBody(response) {
+		mockInitialTokenResponse(suite.mockHTTPClient).Once()
+
+		ctx := context.TODO()
+
+		suite.mockHTTPClient.On("Do", rootMetadataRequestWithInitialToken()).Return(
+			emptyResponse, nil,
+		)
+
+		extractedMetadata, err := cloud.NewAWSMetadata(ctx, suite.mockHTTPClient, suite.cache)
+
+		suite.ErrorContains(err, "failed to fetch metadata. IMDS might be disabled: 403 Forbidden")
+		suite.Nil(extractedMetadata)
+	}
+}
+
+func (suite *AWSMetadataTestSuite) TestRefreshIMDSToken() {
+	unauthorizedResponse := &http.Response{
+		StatusCode: 401,
+	}
+	for _, response := range withEmptyBody(unauthorizedResponse) {
+		ctx := context.TODO()
+
+		suite.mockHTTPClient.On("Do", rootMetadataRequestWithInitialToken()).Return(
+			response, nil,
+		)
+
+		refreshedToken := "refreshedToken"
+
+		mockSuccessfulTokenResponse(suite.mockHTTPClient, refreshedToken).Once()
+
+		mockSuccessfulMetadataDiscovery(suite.mockHTTPClient, func(_ metadataFixtureSpec) any {
+			return requestWithToken(refreshedToken)
+		})
+
+		metadataFetchContext := cloud.NewMetadataFetchContext(suite.cache).WithToken(mockedIMDSToken)
+
+		_, err := cloud.NewAWSMetadataWithFetchContext(ctx, suite.mockHTTPClient, metadataFetchContext)
+
+		suite.NoError(err)
+	}
+}
+
+func (suite *AWSMetadataTestSuite) TestRefreshIMDSTokenExceedsAttempts() {
+	unauthorizedResponse := &http.Response{
+		StatusCode: 401,
+	}
+
+	for _, response := range withEmptyBody(unauthorizedResponse) {
+		ctx := context.TODO()
+
+		suite.mockHTTPClient.On("Do", rootMetadataRequestWithInitialToken()).Return(
+			response, nil,
+		).Once()
+
+		forbiddenResponse := &http.Response{
+			StatusCode: 403,
+		}
+
+		suite.mockHTTPClient.On("Do", fetchTokenRequest()).Return(
+			forbiddenResponse, nil,
+		).Times(3)
+
+		metadataFetchContext := cloud.NewMetadataFetchContext(suite.cache).WithToken(mockedIMDSToken)
+
+		_, err := cloud.NewAWSMetadataWithFetchContext(ctx, suite.mockHTTPClient, metadataFetchContext)
+
+		suite.ErrorContains(err, "failed to fetch metadata: reached maximum token refresh attempts")
+	}
+}
+
+func (suite *AWSMetadataTestSuite) TestNewAWSMetadata() {
+	mockInitialTokenResponse(suite.mockHTTPClient).Once()
+
+	ctx := context.TODO()
+
+	mockSuccessfulMetadataDiscovery(suite.mockHTTPClient, func(fixtureSpec metadataFixtureSpec) any {
+		return matchesRequestPathAndMetadata(fixtureSpec.requestPath, mockedIMDSToken)
+	})
+
+	m, err := cloud.NewAWSMetadata(ctx, suite.mockHTTPClient, suite.cache)
 
 	suite.NoError(err)
 
