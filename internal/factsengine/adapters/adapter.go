02938faf4dd390e835c3cf65315acc26a102f087
arbulu89 <xarbulu@suse.com>
2025-04-08 10:29:18 +0200

    Remove old messaging code

--- internal/factsengine/adapters/adapter.go
+++
@@ -1,9 +0,0 @@
-package adapters
-
-//go:generate mockery --name=Adapter
-
-type Adapter interface {
-	Unsubscribe() error
-	Listen(handle func(contentType string, message []byte) error) error
-	Publish(routingKey, contentType string, message []byte) error
-}
