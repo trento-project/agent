76dedbc26143064b6ceeafb59530a1a0e2373066
Jan Fooken <jan.fooken@suse.com>
2025-05-13 16:47:41 +0200

    Use Host Environment and LC_ALL for Executing Commands

--- pkg/utils/commandexecutor.go
+++ pkg/utils/commandexecutor.go
@@ -3,6 +3,7 @@ package utils
 import (
 	"context"
 	"fmt"
+	"os"
 	"os/exec"
 	"syscall"
 )
@@ -18,14 +19,14 @@ type Executor struct{}
 
 func (e Executor) Exec(name string, arg ...string) ([]byte, error) {
 	cmd := exec.Command(name, arg...)
-	cmd.Env["LC_ALL"] = "C"
+	cmd.Env = append(os.Environ(), "LC_ALL=C")
 
 	return cmd.Output()
 }
 
 func (e Executor) ExecContext(ctx context.Context, name string, arg ...string) ([]byte, error) {
 	cmd := exec.CommandContext(ctx, name, arg...)
-	cmd.Env["LC_ALL"] = "C"
+	cmd.Env = append(os.Environ(), "LC_ALL=C")
 	cmd.SysProcAttr = &syscall.SysProcAttr{Setpgid: true}
 	cmd.Cancel = func() error {
 		err := syscall.Kill(-cmd.Process.Pid, syscall.SIGKILL)
