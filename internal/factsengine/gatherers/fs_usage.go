d978d7c0e3fdf3ad30094b938c93a1de9f0f4db7
Jan Fooken <jan.fooken@suse.com>
2025-05-28 11:14:17 +0200

    Implement file system usage gatherer
    This commit adds a gatherer for determining the usage data of file
    system data.  Most importantly, this allows the user to check for
    capacity, but also for blocks used and available.

---
+++ internal/factsengine/gatherers/fs_usage.go
@@ -0,0 +1,221 @@
+package gatherers
+
+import (
+	"context"
+	"fmt"
+	"regexp"
+	"strconv"
+	"strings"
+
+	log "github.com/sirupsen/logrus"
+	"github.com/trento-project/agent/pkg/factsengine/entities"
+	"github.com/trento-project/agent/pkg/factsengine/plugininterface"
+	"github.com/trento-project/agent/pkg/utils"
+)
+
+const (
+	FSUsageGathererName = "fs-usage"
+)
+
+var (
+	FSUsageInvalidFormatError = entities.FactGatheringError{
+		Type:    "fs-usage-invalid-format-error",
+		Message: "the df output is invalid",
+	}
+
+	FSUsageExecutionError = entities.FactGatheringError{
+		Type:    "fs-usage-execution-error",
+		Message: "the execution of df failed",
+	}
+
+	FSUsageConversionError = entities.FactGatheringError{
+		Type:    "fs-usage-conversion-error",
+		Message: "failed to convert usage information to a fact",
+	}
+)
+
+type FSUsageEntry struct {
+	// Filesystem specifies the type of the filesystem.  This can either be the
+	// device backiing the filesystem or a virtual filesystem like tmpfs.  The
+	// content of this field is dependent on the implementation of df used by the
+	// system.  In SUSE this will most likely be GNU df
+	Filesystem string
+	// Blocks specifies the amount of 1024 byte blocks, the filesystem consists
+	// out of
+	Blocks int
+	// Used specifies how many 1024 byte blocks are used
+	Used int
+	// Available specifies how many 1024 byte blocks are free.  The number may be
+	// negative
+	Available int
+	// Capacity specifies the capacity in percent used by the file system.  The
+	// number may be bigger than 100 when Available is negative
+	Capacity int
+	// Mountpoint specifies the path the filesystem is mounted at
+	Mountpoint string
+}
+
+var _ plugininterface.Gatherer = (*FSUsageGatherer)(nil)
+
+type FSUsageGatherer struct {
+	executor utils.CommandExecutor
+}
+
+func NewFSUsageGatherer(executor utils.CommandExecutor) *FSUsageGatherer {
+	return &FSUsageGatherer{
+		executor: executor,
+	}
+}
+
+func (f *FSUsageGatherer) Gather(ctx context.Context, factsRequests []entities.FactRequest) ([]entities.Fact, error) {
+	log.Infof("Starting %s facts gathering process", FSUsageGathererName)
+	facts := []entities.Fact{}
+
+	for _, factReq := range factsRequests {
+		var data []FSUsageEntry
+
+		if factReq.Argument == "" {
+			result, err := f.gatherAll(ctx)
+			if err != nil {
+				facts = append(facts, entities.NewFactGatheredWithError(factReq, err))
+				continue
+			}
+			data = result
+		} else {
+			result, err := f.gatherSingle(ctx, factReq.Argument)
+			if err != nil {
+				facts = append(facts, entities.NewFactGatheredWithError(factReq, err))
+				continue
+			}
+			data = result
+		}
+
+		factValue, err := fsUsageEntriesToFactValue(data)
+		if err != nil {
+			facts = append(facts, entities.NewFactGatheredWithError(factReq, FSUsageConversionError.Wrap(err.Error())))
+			continue
+		}
+
+		facts = append(facts, entities.NewFactGatheredWithRequest(factReq, factValue))
+	}
+
+	if ctx.Err() != nil {
+		return nil, ctx.Err()
+	}
+
+	log.Infof("Requested %s facts gathered", FSUsageGathererName)
+
+	return facts, nil
+}
+
+// parseFSUsageOutput parses the output of the df -P command
+func (f *FSUsageGatherer) parseFSUsageOutput(b []byte) ([]FSUsageEntry, error) {
+	entries := []FSUsageEntry{}
+
+	lines := strings.Split(string(b), "\n")
+	dfRegex := regexp.MustCompile(`^(.*?) +(\d+) +(\d+) +(-?\d+) +(\d+)% +(.*)$`)
+
+	for i, line := range lines {
+		line = strings.TrimSpace(line)
+		submatches := dfRegex.FindStringSubmatch(line)
+		isMatch := dfRegex.MatchString(line)
+
+		if (!isMatch && i == 0) || line == "" {
+			// Skip parsing the heading or trailing newlines
+			continue
+		}
+
+		if !isMatch || len(submatches) != 7 {
+			return nil, fmt.Errorf("parse df: unexpected field count")
+		}
+
+		filesystem := strings.TrimSpace(submatches[1])
+		// These errors can not happen, because the above matched
+		blocks, err := strconv.Atoi(submatches[2])
+		if err != nil {
+			return nil, fmt.Errorf("parse df: blocks: invalid format: %w", err)
+		}
+		used, err := strconv.Atoi(submatches[3])
+		if err != nil {
+			return nil, fmt.Errorf("parse df: used: invalid format: %w", err)
+		}
+		available, err := strconv.Atoi(submatches[4])
+		if err != nil {
+			return nil, fmt.Errorf("parse df: output: available: invalid format: %w", err)
+		}
+		capacity, err := strconv.Atoi(submatches[5])
+		if err != nil {
+			return nil, fmt.Errorf("parse df: capacity: invalid format: %w", err)
+		}
+		mountpoint := strings.TrimSpace(submatches[6])
+
+		entries = append(entries, FSUsageEntry{
+			Filesystem: filesystem,
+			Blocks:     blocks,
+			Used:       used,
+			Available:  available,
+			Capacity:   capacity,
+			Mountpoint: mountpoint,
+		})
+	}
+
+	return entries, nil
+}
+
+func (f *FSUsageGatherer) gatherAll(ctx context.Context) ([]FSUsageEntry, *entities.FactGatheringError) {
+	// Output in 1024 Blocks
+	content, err := f.executor.ExecContext(ctx, "/usr/bin/df", "-k", "-P", "--")
+	if err != nil {
+		return nil, FSUsageExecutionError.Wrap(err.Error())
+	}
+
+	entries, err := f.parseFSUsageOutput(content)
+	if err != nil {
+		return nil, FSUsageInvalidFormatError.Wrap(err.Error())
+	}
+
+	return entries, nil
+}
+
+func (f *FSUsageGatherer) gatherSingle(ctx context.Context, file string) ([]FSUsageEntry, *entities.FactGatheringError) {
+	// Output in 1024 Blocks
+	content, err := f.executor.ExecContext(ctx, "/usr/bin/df", "-k", "-P", "--", file)
+	if err != nil {
+		return nil, FSUsageExecutionError.Wrap(err.Error())
+	}
+
+	entries, err := f.parseFSUsageOutput(content)
+	if err != nil {
+		return nil, FSUsageInvalidFormatError.Wrap(err.Error())
+	}
+
+	return entries, nil
+}
+
+func (f *FSUsageEntry) ToFactValue() (entities.FactValue, error) {
+	erased := map[string]any{
+		"filesystem": f.Filesystem,
+		"blocks":     f.Blocks,
+		"used":       f.Used,
+		"available":  f.Available,
+		"capacity":   f.Capacity,
+		"mountpoint": f.Mountpoint,
+	}
+
+	return entities.NewFactValue(erased)
+}
+
+func fsUsageEntriesToFactValue(entries []FSUsageEntry) (entities.FactValue, error) {
+	var factValues []entities.FactValue
+
+	for _, entry := range entries {
+		v, err := entry.ToFactValue()
+		if err != nil {
+			return nil, err
+		}
+
+		factValues = append(factValues, v)
+	}
+
+	return &entities.FactValueList{Value: factValues}, nil
+}
