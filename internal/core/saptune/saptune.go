afdc47a956806b5d8bc2e91e2c002a3504cd079f
Antonio Gamez Diaz <antonio.gamez@suse.com>
2026-07-30 16:00:21 +0200

    Discard stderr in runSaptuneJSON
    Signed-off-by: Antonio Gamez Diaz <antonio.gamez@suse.com>

--- internal/core/saptune/saptune.go
+++ internal/core/saptune/saptune.go
@@ -159,7 +159,21 @@ func (s *saptuneClient) runSaptune(ctx context.Context, args ...string) ([]byte,
 }
 
 func (s *saptuneClient) runSaptuneJSON(ctx context.Context, args ...string) ([]byte, error) {
+	slog.Info("Running saptune command with json format", "args", args)
+
 	prependedArgs := append([]string{"--format", "json"}, args...)
 
-	return s.runSaptune(ctx, prependedArgs...)
+	// Using OutputContext instead of CombinedOutputContext to avoid capturing stderr,
+	// which may contain non-JSON output (e.g., warnings) that would break JSON parsing.
+	output, err := s.executor.OutputContext(ctx, "saptune", prependedArgs...)
+	if err != nil {
+		slog.Error("error executing saptune command", "args", prependedArgs, "error", err)
+
+		return output, fmt.Errorf("error executing saptune command: %w", err)
+	}
+
+	slog.Debug("saptune output", "output", string(output))
+	slog.Info("Saptune command executed")
+
+	return output, nil
 }
